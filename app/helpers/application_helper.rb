77a54cc54741c4ad57946db78cc9a6d9a012fd1e
Oliver Valls <oliver.vh@coditramuntana.com>
2020-04-11 17:36:41 +0200

    Add deliveri limit countdown to Solutions index

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,7 @@ module ApplicationHelper
   def ods_options
     @ods_options ||= SDGoal.all.map { |ods| [ods.try(:id).to_s + ". " + translated_attribute(ods.name), ods.id] }
   end
+  def must_render_countdown?
+    DateTime.current < Time.zone.parse('2020-04-13 01:00:00')
+  end
 end
