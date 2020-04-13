e62f877cefbca2065521014f539ca42119028b77
Oliver Valls <oliver.vh@coditramuntana.com>
2020-04-13 14:09:27 +0200

    Use correct "uploads" directory for paperclip

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,8 @@ module DecidimI2catApp
     config.active_record.default_timezone = :local
     config.active_record.time_zone_aware_attributes = false
 
+    config.paperclip_defaults= {url: "/uploads/:class/:attachment/:id_partition/:style/:filename"}
+
     # initializer 'add named route overrides' do |app|
     #   app.routes_reloader.paths << File.expand_path('../named_routes_overrides.rb',__FILE__)
     #   # this seems to cause these extra routes to be loaded last, so they will define named routes last.
