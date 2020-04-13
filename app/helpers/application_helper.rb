c31ed21080dc97a4bdd7c906a6e863ad5a2bf6f0
Oliver Valls <oliver.vh@coditramuntana.com>
2020-04-13 16:50:30 +0200

    Fix, FormBuilder's select tag for sd_goal_id attribute should not be invoked with options_for_select

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@
 
 module ApplicationHelper
   def ods_options
-    @ods_options ||= SDGoal.all.map { |ods| [ods.try(:id).to_s + ". " + translated_attribute(ods.name), ods.id] }
+    @ods_options ||= SDGoal.all.map { |ods| ["#{ods.id}. #{translated_attribute(ods.name)}", ods.id] }
   end
   def must_render_countdown?
     Time.zone.now < Time.zone.parse('2020-04-13 09:00:00')
