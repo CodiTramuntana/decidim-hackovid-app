11a3f373b5afeca6224f60ddbb47a8e9ba7f252a
Oliver Valls <oliver.vh@coditramuntana.com>
2020-04-13 13:41:13 +0200

    Direct link to Solution file

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,6 +5,6 @@ module ApplicationHelper
     @ods_options ||= SDGoal.all.map { |ods| [ods.try(:id).to_s + ". " + translated_attribute(ods.name), ods.id] }
   end
   def must_render_countdown?
-    DateTime.current < Time.zone.parse('2020-04-13 09:00:00')
+    Time.zone.now < Time.zone.parse('2020-04-13 09:00:00')
   end
 end
